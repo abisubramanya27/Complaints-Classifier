5b1ae86b415dd9cd0950984b0b428173dab7ddf6
Abishek <31156690+sabishek@users.noreply.github.com>
2019-08-27 16:30:47 +0530

    Update textsummarization.py

--- textsummarization.py
+++ textsummarization.py
@@ -19,65 +19,69 @@ def idf(word,sentl):
 def tfidf(word,wordl,sentl):
     return tf(word,wordl) * idf(word,sentl)
 
-txt = input("Enter paragraph : ")
+def summarize(txt) :
 
-txt = txt.replace('\'',' ')
-txt = txt.replace('\"',' ')
+    txt = txt.replace('\'',' ')
+    txt = txt.replace('\"',' ')
 
-sentl = list(filter(lambda x:x.strip() != "",re.split('[.?!]+',txt)))
+    sentl = list(filter(lambda x:x.strip() != "",re.split('[.?!]+',txt)))
 
-lemmatizer = WordNetLemmatizer()
+    lemmatizer = WordNetLemmatizer()
 
-place = list(range(len(sentl)))
+    place = list(range(len(sentl)))
 
-clean_sentl = [re.sub('[^a-zA-Z]+',' ',a).lower() for a in sentl]
-clean_sentl = [' '.join(lemmatizer.lemmatize(w) for w in s.split() if (w is not None and w not in stopwords.words('english'))) for s in clean_sentl]
-tmp_l = []
-for ind,s in enumerate(clean_sentl):
-    if s.strip() == "":
-        del place[ind]
-    else:
-        tmp_l.append(s)
+    clean_sentl = [re.sub('[^a-zA-Z]+',' ',a).lower() for a in sentl]
+    clean_sentl = [' '.join(lemmatizer.lemmatize(w) for w in s.split() if (w is not None and w not in stopwords.words('english'))) for s in clean_sentl]
+    tmp_l = []
+    for ind,s in enumerate(clean_sentl):
+        if s.strip() == "":
+            del place[ind]
+        else:
+            tmp_l.append(s)
 
-clean_sentl = tmp_l
+    clean_sentl = tmp_l
 
-wordslist = [w for s in clean_sentl for w in s.split()]
-wordslist = list(np.unique(np.array(wordslist)))
+    wordslist = [w for s in clean_sentl for w in s.split()]
+    wordslist = list(np.unique(np.array(wordslist)))
 
-vector_sent = []
+    vector_sent = []
 
-for s in clean_sentl:
-    score = []
-    wordl = s.split()
-    for w in wordslist:
-        score.append(tfidf(w,wordl,clean_sentl))
-    vector_sent.append(score)
+    for s in clean_sentl:
+        score = []
+        wordl = s.split()
+        for w in wordslist:
+            score.append(tfidf(w,wordl,clean_sentl))
+        vector_sent.append(score)
 
-similarity_matrix = np.zeros((len(clean_sentl),len(clean_sentl)))
-for i in range(len(clean_sentl)):
-    for j in range(len(clean_sentl)):
-        if(i != j):
-            similarity_matrix[i,j] = cosine_similarity(np.array(vector_sent[i]).reshape(1,-1),np.array(vector_sent[j]).reshape(1,-1))
-    if(similarity_matrix[i].sum() == 0):
-        similarity_matrix[i] += (1/len(similarity_matrix))
-    else:
-        similarity_matrix[i] /= similarity_matrix[i].sum()
+    similarity_matrix = np.zeros((len(clean_sentl),len(clean_sentl)))
+    for i in range(len(clean_sentl)):
+        for j in range(len(clean_sentl)):
+            if(i != j):
+                similarity_matrix[i,j] = cosine_similarity(np.array(vector_sent[i]).reshape(1,-1),np.array(vector_sent[j]).reshape(1,-1))
+        if(similarity_matrix[i].sum() == 0):
+            similarity_matrix[i] += (1/len(similarity_matrix))
+        else:
+            similarity_matrix[i] /= similarity_matrix[i].sum()
 
-def TextRank(eps = 0.0001,max_it = 100,d = 0.85):
-    p = np.ones(len(clean_sentl)) / len(clean_sentl)
-    for i in range(max_it):
-        new_p = np.ones_like(p) * (1-d)/len(clean_sentl) + d * (similarity_matrix.T.dot(p))
-        delta = abs(new_p-p).sum()
-        if delta < eps:
-            break
-        p = new_p
-    return p
+    def TextRank(eps = 0.0001,max_it = 100,d = 0.85):
+        p = np.ones(len(clean_sentl)) / len(clean_sentl)
+        for i in range(max_it):
+            new_p = np.ones_like(p) * (1-d)/len(clean_sentl) + d * (similarity_matrix.T.dot(p))
+            delta = abs(new_p-p).sum()
+            if delta < eps:
+                break
+            p = new_p
+        return p
 
-textrank = TextRank()
-textrank = [(score,ind) for ind,score in enumerate(textrank)]
+    textrank = TextRank()
+    textrank = [(score,ind) for ind,score in enumerate(textrank)]
 
-textrank = sorted(textrank,key = lambda x: x[0],reverse = True)
+    textrank = sorted(textrank,key = lambda x: x[0],reverse = True)
 
-print("SUMMARY : ")
-for i in range(int(math.sqrt(len(place)))) :
-    print(sentl[place[textrank[i][1]]])
+    output_str = ""
+
+    for i in range(int(math.sqrt(len(place)))) :
+        output_str += sentl[place[textrank[i][1]]]
+        output_str += " ";
+
+    return output_str
