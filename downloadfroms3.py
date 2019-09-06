9604163bc0bc9841b4dc77ad0ea284160d736635
Abishek <31156690+sabishek@users.noreply.github.com>
2019-09-06 15:45:05 +0530

    Delete downloadfroms3.py

--- downloadfroms3.py
+++
@@ -1,5 +0,0 @@
-import boto3
-bucketname = 'my-bucket' # replace with your bucket name
-filename = 'my_image_in_s3.jpg' # replace with your object key
-s3 = boto3.resource('s3')
-s3.Bucket(bucketname).download_file(filename, 'my_localimage.jpg')
